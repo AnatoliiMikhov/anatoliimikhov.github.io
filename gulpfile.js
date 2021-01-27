2684b715e2117a8949debae2a752769dbbc6c933
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-01-27 21:46:57 +0200

    reformat gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-const gulp = require('gulp');
+const {src, dest, watch, parallel, series} = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
+const {init, stream, reload} = require('browser-sync');
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
 
 // Static server
-gulp.task('server', function () {
-	browserSync.init({
+function server(){
+	init({
 		server: {
 			baseDir: "src"
 		}
 	});
-});
+};
 
-gulp.task('styles', function(){
-	return gulp.src("src/sass/*.+(sass|scss)")
+function styles(){
+	return src("src/sass/*.+(sass|scss)")
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(rename({
 			prefix: "",
@@ -26,13 +26,13 @@ gulp.task('styles', function(){
 			cascade: false
 		}))
 		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(gulp.dest("src/css"))
-		.pipe(browserSync.stream());
-});
+		.pipe(dest("src/css"))
+		.pipe(stream());
+};
 
-gulp.task('watch', function(){
-	gulp.watch('src/sass/*.+(sass|scss)', gulp.parallel('styles'));
-	gulp.watch('src/*html').on('change', browserSync.reload)
-});
+function tracking(){
+	watch('src/sass/*.+(sass|scss)', styles);
+	watch('src/*html').on('change', reload)
+};
 
-gulp.task('default', gulp.parallel('watch', 'styles', 'server'));
+exports.default = parallel(tracking, series(styles, server));
